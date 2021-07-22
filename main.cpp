5c791196878e0e1f46373d8464ba6f9647d0f66b
Liu HongWei <1453701384@qq.com>
2021-07-22 09:56:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "myframelessmainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    //This has the app draw at HiDPI scaling on HiDPI displays, usually two pixels for every one logical pixel
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    
+    //This has QPixmap images use the @2x images when available
+    //See this bug for more details on how to get this right: https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    
+    QApplication a(argc, argv);
+    
+    MyFramelessMainWindow w;
+    w.show();
+    
+    return a.exec();
+}
---
+++ myframelessmainwindow.h
@@ -0,0 +1,72 @@
+﻿#ifndef MYFRAMELESSMAINWINDOW_H
+#define MYFRAMELESSMAINWINDOW_H
+
+#include <QWidget>
+#include <QDesktopWidget>
+#include <QLabel>
+#include <QApplication>
+#include <QEvent>
+#include <QFocusEvent>
+#include <qt_windows.h>
+#include <QWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QDesktopWidget>
+#include <QPushButton>
+
+#include "winnativewindow.h"
+
+#include <iostream>
+using namespace std;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MyFramelessMainWindow; }
+QT_END_NAMESPACE
+
+class MyFramelessMainWindow : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    MyFramelessMainWindow(QWidget *parent = nullptr);
+    ~MyFramelessMainWindow();
+    
+    void initUI(const QString &name);
+    
+    void setTitle(const QString &title);
+    void show();
+    void center();
+    void showCentered();
+    void setGeometry(int x, int y, int w, int h);
+    HWND getParentWindow();
+    
+protected:
+    void childEvent(QChildEvent *e);
+    bool nativeEvent(const QByteArray &, void *message, long *result);
+    bool eventFilter(QObject *o, QEvent *e);
+    void focusInEvent(QFocusEvent *e);
+    bool focusNextPrevChild(bool next);
+    
+private slots:
+    void on_pushButtonMinimum_clicked();
+    
+    void on_pushButtonMaximum_clicked();
+    
+    void on_pushButtonRestore_clicked();
+    
+    void on_pushButtonClose_clicked();
+    
+private:
+    Ui::MyFramelessMainWindow *ui;
+    
+    void resetFocus();
+    void saveFocus();
+    
+    WinNativeWindow *p_ParentWinNativeWindow;
+    HWND m_ParentNativeWindowHandle;
+    int BORDERWIDTH = 6;	//Adjust this as you wish for # of pixels on the edges to show resize handles
+    int TOOLBARHEIGHT = 80; //Adjust this as you wish for # of pixels from the top to allow dragging the window
+    HWND _prevFocus;
+    bool _reenableParent;
+};
+#endif // MYFRAMELESSMAINWINDOW_H
